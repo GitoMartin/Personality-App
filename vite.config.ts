0cdc705f36543e8f98eaa1d358603c6c5070afc1
GitoGMartin <gito.gm18@gmail.com>
2026-05-10 23:22:27 +0200

    this too

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
+
 export default defineConfig({
-  vite: {
-    // keep this empty unless you need overrides
-  },
+  base: "/",
 });
